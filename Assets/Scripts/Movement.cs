4754fe2c5c6a712789deb834e99c73044a57b93d
asassin1901 <57683620+asassin1901@users.noreply.github.com>
2022-01-09 20:40:56 +0100

    Top_Down_Shooting
    Added:
    Shooting
    Rotation
    Temp. Bullet Prefab
    Temp. Bullet hit Prefab
    Basic Bullet behaviour
    Temp. Basic Gun
    
    To Do:
    Dashing

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Camera cam;
+
     public float moveSpeed;
 
     private Vector2 moveDirection;
+    private Vector2 mousePos;
 
     // We Process Inputs here
     void Update()
     {
         ProcessInputs();
     }
-    // We move player character here
+    // We move and rotate player character here
     private void FixedUpdate()
     {
         Move();
+        Rotate();
     }
 
     void ProcessInputs()
@@ -26,10 +30,20 @@ public class Movement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Horizontal");
 
         moveDirection = new Vector2(moveY, moveX).normalized;
+
+        //Where the mouse is
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void Move()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Rotate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
