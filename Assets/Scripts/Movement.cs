1d3d9ebf94396f834b52859dd6fbc43462d79f2d
asassin1901 <57683620+asassin1901@users.noreply.github.com>
2022-01-12 16:58:36 +0100

    Dashing
    Dashing is now implemented. It needs quite a few tweaks but it is possible to dash now

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,7 +7,15 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     public Camera cam;
 
+    //How fast we're moving
     public float moveSpeed;
+    //To keep it simple this is how far we're dashing
+    public float dashRange;
+
+    //If this is false we're not dashing
+    public bool dash;
+    //Are we taking damage
+    public bool HP = true;
 
     private Vector2 moveDirection;
     private Vector2 mousePos;
@@ -22,6 +30,7 @@ public class Movement : MonoBehaviour
     {
         Move();
         Rotate();
+        Dash();
     }
 
     void ProcessInputs()
@@ -29,10 +38,14 @@ public class Movement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Vertical");
         float moveY = Input.GetAxisRaw("Horizontal");
 
+        //Where are we moving
         moveDirection = new Vector2(moveY, moveX).normalized;
 
         //Where the mouse is
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //We dashin here
+        dash = Input.GetKey(KeyCode.Space);
     }
 
     void Move()
@@ -45,5 +58,22 @@ public class Movement : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+    }
+
+    void Dash()
+    {
+        if (dash)
+        {
+            HP = false;
+            rb.MovePosition(rb.position + moveDirection * dashRange * Time.deltaTime);
+            StartCoroutine(iFrames());
+        }
+    }
+
+    IEnumerator iFrames()
+    {
+        HP = false;
+        yield return new WaitForSeconds(0.4f);
+        HP = true;
     }
 }
